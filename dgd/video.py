2be2a47b8bcd3f090c51f3cdba496eb2c7e6b6cb
Bosco Yung <15840328+bhky@users.noreply.github.com>
2022-04-30 20:35:01 +0800

    Draw detection on frame when using webcam

--- dgd/video.py
+++ dgd/video.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 
 import cv2
 from mediapipe.python.solutions import face_detection as mp_face
+from mediapipe.python.solutions import drawing_utils as mp_drawing
 
 
 def video_to_landmarks(
@@ -56,20 +57,23 @@ def video_to_landmarks(
 
             valid_frame_count += 1
 
-            text = f"{valid_frame_count} / {max_num_frames}"
-            cv2.putText(  # pylint: disable=no-member
-                frame, text, (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX,  # pylint: disable=no-member
-                font_scale, (0, 0, 255),
-                line_thickness, cv2.LINE_AA  # pylint: disable=no-member
-            )
-            cv2.imshow(  # pylint: disable=no-member
-                "Video",
-                cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # pylint: disable=no-member
-            )
+            if video_path == 0:
+                mp_drawing.draw_detection(frame, detection)
 
-            if video_path == 0 and cv2.waitKey(1) == ord('q'):
-                break
+                text = f"{valid_frame_count} / {max_num_frames}"
+                cv2.putText(  # pylint: disable=no-member
+                    frame, text, (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX,  # pylint: disable=no-member
+                    font_scale, (0, 0, 255),
+                    line_thickness, cv2.LINE_AA  # pylint: disable=no-member
+                )
+                cv2.imshow(  # pylint: disable=no-member
+                    "Video",
+                    cv2.flip(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR), 1)  # pylint: disable=no-member
+                )
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
 
     if padding and max_num_frames and len(landmarks) < max_num_frames:
         zeros = [0.0] * 12
