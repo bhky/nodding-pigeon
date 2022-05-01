de793097fabbe7eade7b7d934e0be94cd936a9d0
Bosco Yung <15840328+bhky@users.noreply.github.com>
2022-05-01 16:30:03 +0800

    Fix predict_video func

--- dgd/inference.py
+++ dgd/inference.py
@@ -7,6 +7,7 @@ from typing import Optional, Sequence
 from tensorflow.keras.models import Model
 
 from dgd._download import get_default_weights_path, download_weights_to
+from dgd.config import Config
 from dgd.model import make_model
 from dgd.video import video_to_landmarks
 
@@ -21,10 +22,12 @@ def load_pretrained_model(weights_path: str = get_default_weights_path()) -> Mod
 
 def predict_video(
         video_path: Optional[str] = None,  # None will start a webcam.
-        model: Model = load_pretrained_model(),
-        max_num_frames: int = 300,  # For the pre-trained model.
+        model: Optional[Model] = None,
+        max_num_frames: int = Config.seq_length,  # For the pre-trained model.
         padding: bool = True
 ) -> Sequence[float]:
+    if model is None:
+        model = load_pretrained_model()
     landmarks = video_to_landmarks(video_path, max_num_frames, padding)
-    prediction: Sequence[float] = model.predict([landmarks])[0]
+    prediction: Sequence[float] = model.predict([landmarks])[0].tolist()
     return prediction
