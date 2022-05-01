7ca457634864e19239206a74da50f14f11eda0a7
Bosco Yung <15840328+bhky@users.noreply.github.com>
2022-05-01 16:57:22 +0800

    Use two classes

--- dgd/config.py
+++ dgd/config.py
@@ -7,7 +7,7 @@ from typing import Sequence
 
 @dataclass
 class Config:
-    labels: Sequence[str] = ("head_nodding", "head_turning", "head_undefined")
+    labels: Sequence[str] = ("head_nodding", "head_turning")
     npz_filename: str = "landmarks.npz"
     seq_length: int = 100
     num_features: int = 12
