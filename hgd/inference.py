f6a9fea8f3754f5decaf1c6c2c7cb5280ad7ca73
Bosco Yung <15840328+bhky@users.noreply.github.com>
2022-05-04 09:17:02 +0800

    Add postprocessing

--- hgd/inference.py
+++ hgd/inference.py
@@ -2,7 +2,7 @@
 Inference utilities.
 """
 import os
-from typing import Callable, List, Optional, Sequence
+from typing import Any, Callable, Dict, List, Optional, Sequence
 
 import numpy as np
 from tensorflow.keras.models import Model
@@ -22,20 +22,40 @@ def load_pretrained_model(weights_path: str = get_default_weights_path()) -> Mod
     return model
 
 
+def postprocess(
+        prediction: Sequence[float],
+        is_moving_threshold: float = 0.5,
+        class_threshold: float = 0.9
+) -> Dict[str, Any]:
+    if prediction[0] < is_moving_threshold:
+        label = Config.stationary_label
+    else:
+        class_probs = prediction[1:]
+        if np.max(class_probs) < class_threshold:
+            label = Config.undefined_class_label
+        else:
+            label = Config.class_labels[int(np.argmax(class_probs))]
+    return {
+        "gesture": label,
+        "is_moving_probability": prediction[0],
+        "gesture_probabilities": prediction[1:]
+    }
+
+
 def predict_video(
         video_path: Optional[str] = None,  # None will start a webcam.
         model: Optional[Model] = None,
         max_num_frames: int = Config.seq_length,  # For the pre-trained model.
         padding: bool = True,
         preprocess_fn: Callable[[List[List[float]]], NDFloat32Array] = preprocess
-) -> Sequence[float]:
+) -> Dict[str, Any]:
     if model is None:
         model = load_pretrained_model()
     landmarks = video_to_landmarks(video_path, max_num_frames, padding)
     prediction: Sequence[float] = model.predict(
         np.expand_dims(preprocess_fn(landmarks), axis=0)
     )[0].tolist()
-    return prediction
+    return postprocess(prediction)
 
 
 # if __name__ == "__main__":
