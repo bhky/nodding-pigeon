96d638cfd197659f25e88d4571cba46904be0adb
katsunori-waragai <huyuhiko1128@gmail.com>
2023-11-24 00:42:14 +0900

    modified sample

--- videofile.py
+++ videofile.py
@@ -1,16 +1,17 @@
 from noddingpigeon.inference import predict_video
 from noddingpigeon.video import VideoSegment  # Optional.
 
-import argparse
+if __name__ == "__main__":
+  import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("name", help="video file name")
-args = parser.parse_args()
-videoname = args.name
-result = predict_video(
-  videoname,
-  video_segment=VideoSegment.LAST,  # Optionally change these parameters.
-  motion_threshold=0.5,
-  gesture_threshold=0.9
-)
-print(result)
+  parser = argparse.ArgumentParser()
+  parser.add_argument("name", help="video file name")
+  args = parser.parse_args()
+  videoname = args.name
+  result = predict_video(
+    videoname,
+    video_segment=VideoSegment.LAST,  # Optionally change these parameters.
+    motion_threshold=0.5,
+    gesture_threshold=0.9
+  )
+  print(result)
--- webcam.py
+++ webcam.py
@@ -1,10 +1,11 @@
 from noddingpigeon.inference import predict_video
 
-result = predict_video()
-print(result)
-# Example result:
-# {'gesture': 'nodding',
-#  'probabilities': {'has_motion': 1.0,
-#   'gestures': {'nodding': 0.9576354622840881,
-#    'turning': 0.042364541441202164}}}
+while True:
+    result = predict_video()
+    print(result)
+    # Example result:
+    # {'gesture': 'nodding',
+    #  'probabilities': {'has_motion': 1.0,
+    #   'gestures': {'nodding': 0.9576354622840881,
+    #    'turning': 0.042364541441202164}}}
 
